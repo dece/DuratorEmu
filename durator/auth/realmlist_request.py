de6de594d77c4590d62add1ac19a45a134d5efc9
Shgck <shgck@pistache.land>
2015-12-09 00:31:52 +0100

    Working empty realm list

--- durator/auth/realmlist_request.py
+++ durator/auth/realmlist_request.py
@@ -3,7 +3,7 @@ from durator.auth.constants import LoginOpCodes
 
 class RealmlistRequest(object):
 
-    HEADER_SIZE = 7
+    MIN_RESPONSE_SIZE = 7
 
     def __init__(self, connection, packet):
         self.conn = connection
@@ -13,7 +13,7 @@ class RealmlistRequest(object):
         realm_list = self.conn.server.get_realm_list()
         realm_list = {}
         realm_infos = self._get_realm_info_packets(realm_list)
-        response = self._add_realm_infos_header(realm_infos, len(realm_list))
+        response = self._get_full_realm_infos(realm_infos, len(realm_list))
         return None, response
 
     def _get_realm_info_packets(self, realm_list):
@@ -35,11 +35,14 @@ class RealmlistRequest(object):
         # packet += b"\x00"  # unknown ?
         return packet
 
-    def _add_realm_infos_header(self, realm_infos, num_realms):
+    def _get_full_realm_infos(self, realm_infos, num_realms):
         header = b""
         header += int.to_bytes(LoginOpCodes.REALMLIST.value, 1, "little")
-        full_packet_size = RealmlistRequest.HEADER_SIZE + len(realm_infos)
+        full_packet_size = RealmlistRequest.MIN_RESPONSE_SIZE + len(realm_infos)
         header += int.to_bytes(full_packet_size, 2, "little")
         header += int.to_bytes(0, 4, "little")  # unknown
         header += int.to_bytes(num_realms, 1, "little")
-        return header + realm_infos
+
+        footer = int.to_bytes(5, 2, "little")  # unknown
+
+        return header + realm_infos + footer
