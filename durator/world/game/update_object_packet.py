6b4a62cd5ae634b3c547c8aeaac6837a40910819
Shgck <shgck@pistache.land>
2016-01-17 06:24:31 +0100

    Clean and finalize and alpha version of the UpdateObjectPacket (still WIP)

--- durator/world/game/update_object_packet.py
+++ durator/world/game/update_object_packet.py
@@ -4,6 +4,8 @@ from enum import Enum
 import math
 from struct import Struct
 
+from durator.world.game.char.constants import NEW_CHAR_CONSTS
+from durator.world.game.object.base_object import ObjectType
 from durator.world.game.object.object_fields import (
     ObjectField, UnitField, PlayerField )
 from durator.world.game.object.object_fields_type import (
@@ -24,42 +26,85 @@ class UpdateType(Enum):
 
 
 class UpdateObjectPacket(WorldPacket):
+    """ WIP """
+
+    UPDATE_TYPES_WITH_FIELDS = ( UpdateType.PARTIAL
+                               , UpdateType.MOVEMENT
+                               , UpdateType.CREATE_OBJECT )
 
     # uint32  count
     # uint8   bool hasTransport (?)
     # uint8   UPDATE_TYPE
     # uint64  guid
     # uint8   OBJECT_TYPE
-    PACKET_PART1_BIN    = Struct("<I2BQB")
+    PACKET_PART1_BIN = Struct("<I2BQB")
 
     # uint32    flags
     # uint32    unk
     # float[4]  position+ori
     # float[6]  speeds (walk, run, bw, swim, swim bw, turn)
     # may be more complete with some flags
-    PACKET_MOVEMENT_FMT = Struct("<2I4f6f")
+    PACKET_MOVEMENT_FMT = "<2I4f6f"
 
     # uint32  isPlayer ? 1 : 0
     # uint32  attack cycle
     # uint32  timer ID
     # uint64  victim guid
-    PACKET_PART2_BIN    = Struct("<3IQ")
+    PACKET_PART2_BIN = Struct("<3IQ")
 
-    def __init__(self, data = None):
-        super().__init__(self, data)
+    def __init__(self, update_type, update_infos):
+        super().__init__(data = None)
         self.opcode = OpCode.SMSG_UPDATE_OBJECT
+        self.update_type = update_type
+        self.update_infos = update_infos
 
-        self.blocks_builder = UpdateBlocksBuilder()
+        if self.update_type != UpdateType.CREATE_OBJECT:
+            raise NotImplementedError()
+
+        self.has_fields = self.update_type in self.UPDATE_TYPES_WITH_FIELDS
+        self.blocks_builder = UpdateBlocksBuilder() if self.has_fields else None
 
     def add_field(self, field, value):
+        """ If this update packet can hold update fields, add it. """
+        if not self.has_fields:
+            LOG.error("Tried to add an update field to a wrong update packet.")
+            return
         self.blocks_builder.add(field, value)
 
-    def create_data(self):
-        """ Create binary data from the packet information.
-        Required to call to_bytes. """
-        update_blocks = self.blocks_builder.to_bytes()
-        # TODO complete
-        self.data = update_blocks
+    def to_socket(self, session_cipher = None):
+        """ WIP, this stuff still shouldn't be hardcoded """
+        data = b""
+
+        # The update_infos dict should contain the player field at least for
+        # type 2 updates, and maybe some others.
+        player = self.update_infos["player"]
+        guid = player.get(ObjectField.GUID)
+        data += self.PACKET_PART1_BIN.pack(
+            1, 0, self.update_type.value, guid, ObjectType.PLAYER.value
+        )
+
+        # Assumes no flag for the movement
+        movement_struct = Struct(self.PACKET_MOVEMENT_FMT.format())
+        data += movement_struct.pack(
+            0, 0,
+            player.coords["x"],
+            player.coords["y"],
+            player.coords["z"],
+            player.coords["o"],
+            NEW_CHAR_CONSTS["speed_walk"],
+            NEW_CHAR_CONSTS["speed_run"],
+            NEW_CHAR_CONSTS["speed_run_bw"],
+            NEW_CHAR_CONSTS["speed_swim"],
+            NEW_CHAR_CONSTS["speed_swim_bw"],
+            NEW_CHAR_CONSTS["speed_turn"]
+        )
+
+        data += self.PACKET_PART2_BIN.pack(1, 1, 0, 0)
+
+        data += self.blocks_builder.to_bytes()
+
+        self.data = data
+        return super().to_socket(session_cipher)
 
 
 class UpdateBlocksBuilder(object):
@@ -73,11 +118,14 @@ class UpdateBlocksBuilder(object):
         FieldType.FOUR_BYTES: Struct("<I")
     }
 
+    HARD_MASK_BLOCKS_LIMIT = 0x1C
+
     def __init__(self):
         self.mask_blocks = []
         self.update_blocks = []
 
     def add(self, field, value):
+        """ Add a field and its value to the UpdateBlocks. """
         try:
             field_type = FIELD_TYPE_MAP[field]
         except KeyError:
@@ -90,6 +138,13 @@ class UpdateBlocksBuilder(object):
 
         update_block = field_struct.pack(value)
         self.update_blocks.append(update_block)
+
+        num_mask_blocks = len(self.mask_blocks)
+        if num_mask_blocks >= self.HARD_MASK_BLOCKS_LIMIT:
+            LOG.critical("Too much update mask blocks ({} >= {})".format(
+                num_mask_blocks, self.HARD_MASK_BLOCKS_LIMIT
+            ))
+            raise Exception()
 
     def _set_field_mask_bits(self, field, field_struct):
         num_mask_blocks = math.ceil(field_struct.size / 4)
@@ -104,16 +159,19 @@ class UpdateBlocksBuilder(object):
         self.mask_blocks[mask_block_index] |= 1 << bit_index
 
     def to_bytes(self):
-        mask = b"".join(
-            [int.to_bytes(block, 4, "little") for block in self.mask_blocks]
-        )
-        update_data = b"".join(
-            self.update_blocks
-        )
-        return mask + update_data
+        """ Return the mask count, the mask and the update blocks as bytes. """
+        num_mask_blocks_bytes = int.to_bytes(len(self.mask_blocks), 1, "little")
+        mask_blocks = [int.to_bytes(b, 4, "little") for b in self.mask_blocks]
+        mask_bytes = b"".join(mask_blocks)
+        update_data = b"".join(self.update_blocks)
+        return num_mask_blocks_bytes + mask_bytes + update_data
 
 
 
+
+
+# These values are enough to let the client make the player show in world.
+# There may be a lot of superfluous values but well...
 PLAYER_SPAWN_FIELDS = [
     ObjectField.GUID,
     ObjectField.TYPE,
