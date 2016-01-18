461b9af56ee3d4f4b9faef23e3add03ae1f15183
Shgck <shgck@pistache.land>
2016-01-18 06:04:55 +0100

    Binary interface for Position and Movement classes

--- durator/world/game/movement.py
+++ durator/world/game/movement.py
@@ -1,18 +1,20 @@
 from enum import Enum
+import io
 from struct import Struct
 
 from durator.world.game.position import Position
+from pyshgck.bin import read_struct
 
 
 class MovementFlags(Enum):
     """ Flags defining the MovementBlock structure. Need double checking. """
 
-    # FORWARD      = 1 << 0
-    # BACKWARD     = 1 << 1
-    # STRAFE_LEFT  = 1 << 2
-    # STRAFE_RIGHT = 1 << 3
-    # TURN_LEFT    = 1 << 4
-    # TURN_RIGHT   = 1 << 5
+    FORWARD      = 1 << 0
+    BACKWARD     = 1 << 1
+    STRAFE_LEFT  = 1 << 2
+    STRAFE_RIGHT = 1 << 3
+    TURN_LEFT    = 1 << 4
+    TURN_RIGHT   = 1 << 5
     # PITCH_UP     = 1 << 6
     # PITCH_DOWN   = 1 << 7
     # WALK_MODE    = 1 << 8
@@ -35,12 +37,39 @@ class MovementFlags(Enum):
     # HOVER            = 1 << 30
 
 
-class MovementBlock(object):
+class JumpData(object):
+
+    BIN = Struct("<I4f")
+
+    def __init__(self):
+        self.time = 0
+        self.velocity = 0.0
+        self.sin = 0.0
+        self.cos = 0.0
+        self.xy_speed = 0.0
+
+    @staticmethod
+    def from_io(bytes_io):
+        jump = JumpData()
+        data = read_struct(bytes_io, JumpData.BIN)
+        jump.time, jump.velocity, jump.sin, jump.cos, jump.xy_speed = data
+        return jump
+
+    def to_bytes(self):
+        return self.BIN.pack(
+            self.time,
+            self.velocity,
+            self.sin,
+            self.cos,
+            self.xy_speed
+        )
+
+
+class Movement(object):
 
     HEADER_BIN           = Struct("<2I")
     TRANSPORT_HEADER_BIN = Struct("<Q")
     SWIMMING_BIN         = Struct("<f")
-    FALLING_BIN          = Struct("<I4f")
     SPLINE_ELEVATION_BIN = Struct("<f")
 
     def __init__(self):
@@ -50,12 +79,35 @@ class MovementBlock(object):
         self.transport_guid = 0
         self.transport_position = Position()
         self.swim_pitch = 0.0
-        self.fall_time = 0
-        self.jump_velocity = 0.0
-        self.jump_sin = 0.0
-        self.jump_cos = 0.0
-        self.jump_xy_speed = 0.0
+        self.jump_data = JumpData()
         self.spline_elevation_unk = 0.0
+
+    @staticmethod
+    def from_bytes(data):
+        movement = Movement()
+        data_io = io.BytesIO(data)
+
+        header_data = read_struct(data_io, Movement.HEADER_BIN)
+        movement.flags, movement.time = header_data
+        movement.position = Position.from_io(data_io)
+
+        if movement.flags & MovementFlags.ON_TRANSPORT.value:
+            transport_data = read_struct(data_io, Movement.TRANSPORT_HEADER_BIN)
+            movement.transport_guid = transport_data[0]
+            movement.transport_position = Position.from_io(data_io)
+
+        if movement.flags & MovementFlags.IS_SWIMMING.value:
+            swimming_data = read_struct(data_io, Movement.SWIMMING_BIN)
+            movement.swim_pitch = swimming_data[0]
+
+        if movement.flags & MovementFlags.IS_FALLING.value:
+            movement.jump_data = JumpData.from_io(data_io)
+
+        if movement.flags & MovementFlags.SPLINE_ELEVATION.value:
+            elevation_data = read_struct(data_io, Movement.SPLINE_ELEVATION_BIN)
+            movement.spline_elevation_unk = elevation_data[0]
+
+        return movement
 
     def to_bytes(self):
         data = b""
@@ -70,13 +122,7 @@ class MovementBlock(object):
             data += self.SWIMMING_BIN.pack(self.swim_pitch)
 
         if self.flags & MovementFlags.IS_FALLING.value:
-            data += self.FALLING_BIN.pack(
-                self.fall_time,
-                self.jump_velocity,
-                self.jump_sin,
-                self.jump_cos,
-                self.jump_xy_speed
-            )
+            data += self.jump_data.to_bytes()
 
         if self.flags & MovementFlags.SPLINE_ELEVATION.value:
             data += self.SPLINE_ELEVATION_BIN.pack(self.spline_elevation_unk)
--- durator/world/game/position.py
+++ durator/world/game/position.py
@@ -1,5 +1,7 @@
 from struct import Struct
 
+from pyshgck.bin import read_struct
+
 
 class Position(object):
 
@@ -10,6 +12,16 @@ class Position(object):
         self.y = 0.0
         self.z = 0.0
         self.o = 0.0
+
+    @staticmethod
+    def from_io(bytes_io):
+        position = Position()
+        position_data = read_struct(bytes_io, Position.BIN)
+        position.x = position_data[0]
+        position.y = position_data[1]
+        position.z = position_data[2]
+        position.o = position_data[3]
+        return position
 
     def to_bytes(self):
         return self.BIN.pack(self.x, self.y, self.z, self.o)
