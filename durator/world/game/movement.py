7a2ae10ed990180242168001121ccca8ad20bec8
Shgck <shgck@pistache.land>
2016-01-31 03:39:46 +0100

    Clean Movement module

--- durator/world/game/movement.py
+++ durator/world/game/movement.py
@@ -9,63 +9,52 @@ from pyshgck.bin import read_struct
 class MovementFlags(Enum):
     """ Flags defining the MovementBlock structure. Need double checking. """
 
-    FORWARD      = 1 << 0
-    BACKWARD     = 1 << 1
-    STRAFE_LEFT  = 1 << 2
-    STRAFE_RIGHT = 1 << 3
-    TURN_LEFT    = 1 << 4
-    TURN_RIGHT   = 1 << 5
-    # PITCH_UP     = 1 << 6
-    # PITCH_DOWN   = 1 << 7
-    # WALK_MODE    = 1 << 8
+    FORWARD          = 1 << 0   # 0x00000001
+    BACKWARD         = 1 << 1   # 0x00000002
+    STRAFE_LEFT      = 1 << 2   # 0x00000004
+    STRAFE_RIGHT     = 1 << 3   # 0x00000008
+    TURN_LEFT        = 1 << 4   # 0x00000010
+    TURN_RIGHT       = 1 << 5   # 0x00000020
+    IS_FALLING       = 1 << 13  # 0x00002000
+    IS_SWIMMING      = 1 << 21  # 0x00200000
+    ON_TRANSPORT     = 1 << 25  # 0x02000000
+    SPLINE_ELEVATION = 1 << 26  # 0x04000000
 
-    # IS_LEVITATING  = 1 << 10
-    # IS_FLYING      = 1 << 11
-    IS_FALLING     = 1 << 13
-    IS_FALLING_FAR = 1 << 14
-    IS_SWIMMING    = 1 << 21
+    # Values from 1.12 (or 3.3.5?) left here for documentation
 
-    # SPLINE_ENABLED = 1 << 22
-    # CAN_FLY        = 1 << 23
-    # FLYING_OLD     = 1 << 24
-
-    ON_TRANSPORT     = 1 << 25
-    SPLINE_ELEVATION = 1 << 26
+    # PITCH_UP         = 1 << 6
+    # PITCH_DOWN       = 1 << 7
+    # WALK_MODE        = 1 << 8
+    # IS_LEVITATING    = 1 << 10
+    # IS_FLYING        = 1 << 11
+    # SPLINE_ENABLED   = 1 << 22
+    # IS_FALLING_FAR   = 1 << 14
+    # CAN_FLY          = 1 << 23
+    # FLYING_OLD       = 1 << 24
     # ROOT             = 1 << 27
     # IS_WATERWALKING  = 1 << 28
     # SAFE_FALL        = 1 << 29
     # HOVER            = 1 << 30
 
 
-class JumpData(object):
-
-    BIN = Struct("<I4f")
-
-    def __init__(self):
-        self.time = 0
-        self.velocity = 0.0
-        self.sin = 0.0
-        self.cos = 0.0
-        self.xy_speed = 0.0
-
-    @staticmethod
-    def from_io(bytes_io):
-        jump = JumpData()
-        data = read_struct(bytes_io, JumpData.BIN)
-        jump.time, jump.velocity, jump.sin, jump.cos, jump.xy_speed = data
-        return jump
-
-    def to_bytes(self):
-        return self.BIN.pack(
-            self.time,
-            self.velocity,
-            self.sin,
-            self.cos,
-            self.xy_speed
-        )
-
-
 class Movement(object):
+
+    # - uint32      flags
+    # - int32       unk
+    # - float[4]    position + ori
+    #     if flags & 0x02000000 (on transport)
+    #     - uint64      guid
+    #     - float[4]    tr_position + tr_ori
+    #     if flags & 0x00200000 (is swimming)
+    #     - uint32      swim_pitch
+    #     if flags & 0x00002000 (is jumping/falling)
+    #     - uint32      time
+    #     - float       velocity
+    #     - float       sin
+    #     - float       cos
+    #     - float       xy_speed
+    #     if flags & 0x04000000 (is on spline elevation)
+    #     - float       unk
 
     HEADER_BIN           = Struct("<2I")
     TRANSPORT_HEADER_BIN = Struct("<Q")
@@ -111,7 +100,7 @@ class Movement(object):
 
     def to_bytes(self):
         data = b""
-        data += self.HEADER_BIN(self.flags, self.time)
+        data += self.HEADER_BIN.pack(self.flags, self.time)
         data += self.position.to_bytes()
 
         if self.flags & MovementFlags.ON_TRANSPORT.value:
@@ -128,3 +117,31 @@ class Movement(object):
             data += self.SPLINE_ELEVATION_BIN.pack(self.spline_elevation_unk)
 
         return data
+
+
+class JumpData(object):
+
+    BIN = Struct("<I4f")
+
+    def __init__(self):
+        self.time = 0
+        self.velocity = 0.0
+        self.sin = 0.0
+        self.cos = 0.0
+        self.xy_speed = 0.0
+
+    @staticmethod
+    def from_io(bytes_io):
+        jump = JumpData()
+        data = read_struct(bytes_io, JumpData.BIN)
+        jump.time, jump.velocity, jump.sin, jump.cos, jump.xy_speed = data
+        return jump
+
+    def to_bytes(self):
+        return self.BIN.pack(
+            self.time,
+            self.velocity,
+            self.sin,
+            self.cos,
+            self.xy_speed
+        )
